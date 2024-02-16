ac4e2e0a730bd18c4047d7d44146b6ee0770a15e
Kanya-AngularMinds <kanya@angularminds.in>
2024-02-16 10:10:58 +0530

    adjusted banner image

--- src/components/banner/RightBanner.js
+++ src/components/banner/RightBanner.js
@@ -5,11 +5,11 @@ const RightBanner = () => {
   return (
     <div className="relative flex items-center justify-center w-full lgl:w-1/2">
       <img
-        className="w-[300px] h-auto lgl:w-[500px] lgl:h-[580px] z-10 object-fill"
+        className="w-[300px] h-auto lgl:w-[500px] lgl:h-[380px] z-10 object-contain rounded"
         src={Banner}
         alt="bannerImg"
       />
-      <div className="absolute bottom-0 w-[350px] h-[300px] lgl:w-[500px] lgl:h-[500px] bg-gradient-to-r from-[#1e2024] to-[#202327] shadow-shadowOne flex justify-center items-center"></div>
+      <div className="absolute bottom-0 w-[350px] h-[300px] lgl:w-[500px] rounded lgl:h-auto bg-gradient-to-r from-[#1e2024] to-[#202327] shadow-shadowOne flex justify-center items-center"></div>
     </div>
   );
 };
